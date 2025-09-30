ef4f53f6a96fa397a0a16d1ed9533cf89d38563e
XHoang04 <157344845+XHoang04@users.noreply.github.com>
2025-09-30 23:06:30 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,ts}",
+    "./pages/**/*.{js,ts,jsx,tsx,ts}",
+    "./components/**/*.{js,ts,jsx,tsx,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
